fc76d727d65dbcc54859053005402343b4c012f1
Digendra Kumar Sahu <50202639+DigendraSahu@users.noreply.github.com>
2021-02-14 17:32:43 +0530

    Update dataStructures.py

--- dataStructures.py
+++ dataStructures.py
@@ -1,10 +1,12 @@
 # In a given list of elements, all elements are equal except the one.Write a code to find the odd man out (Stray number)
 def odd_man_out(l1):
-    for i in range(len(l1)):
-        for j in range(len(l1)):
-            if l1[i]!=l1[j]:
-                return l1[j]
-
+    if l1[0]!=l1[1] and l1[2]==l1[2]:
+        return l1[0]
+    else:
+        for i in range(len(l1)):
+            for j in range(len(l1)):
+                if l1[i]!=l1[j]:
+                    return l1[j]
 # In a given list of elements, find the elements which is close to its mean
 def close_to_mean(l1):
     minimum = 10000
@@ -16,7 +18,10 @@ def close_to_mean(l1):
     return ans
 
 # Find the average speed of vehicle, given the distance travelled for fixed time intervals, e.g. [0, 0.1, 0.25, 0.45, 0.55, 0.7, 0.9, 1.0]
-def avg_speed(l1):
+def avg_speed(l):
+    l1 = []
+    for i in range(0,len(l)-1):
+        l1.append(l[i+1]-l[i])
     return sum(l1)/len(l1)
 
 # Find the no.of people in a bus, given the data of people onboarding & alighting at each station
